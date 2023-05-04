b0195e153c4ad2d8acb98fa7398a0e8752570d52
Muhammed Ashique <muhammedashiques2017@gmail.com>
2023-05-04 14:58:57 +0530

    added lazy column to render 100 list items

--- app/src/main/java/com/experion/sampleapp/MainActivity.kt
+++ app/src/main/java/com/experion/sampleapp/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
@@ -18,8 +21,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -34,68 +40,107 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SampleAppTheme {
-                // A surface container using the 'background' color from the theme
 
-                Surface(
-                    color = Color.White
-                ) {
-                    MyApp()
-                }
-
+                MyApp(modifier = Modifier.fillMaxSize())
             }
+        }
+    }
+
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greetings()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val expanded = remember{ mutableStateOf(false) }
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Composable
+private fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(100){"$it"}
+) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names){
+            name -> Greeting(name = name)
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+
+        OnboardingScreen(onContinueClicked = {})
+    }
+
+
+@Composable
+private fun Greeting(name: String) {
+
+    val expanded = remember { mutableStateOf(false) }
+
     val extraPadding = if (expanded.value) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row (modifier = Modifier.padding(24.dp)){
-            Column(
-                modifier = Modifier
-
-                    .weight(1f)
-                    .padding(bottom = extraPadding)
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)
             ) {
-                Text(text = "Hello,")
+                Text(text = "Hello, ")
                 Text(text = name)
             }
             ElevatedButton(
-                onClick = { expanded.value = !expanded.value },
-
+                onClick = { expanded.value = !expanded.value }
             ) {
-                Text(if (expanded.value) "show less" else "show more")
-                println("expanded $expanded")
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
 }
 
-
+@Preview(showBackground = true, widthDp = 320)
 @Composable
-fun MyApp(
-    modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
-) {
-    Column(
+fun DefaultPreview() {
 
-    ) {
-        for (name in names) {
-            Greeting(name = name)
-        }
-
+        Greetings()
     }
-}
 
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    MyApp()
-}
+fun MyAppPreview() {
+
+        MyApp(Modifier.fillMaxSize())
+    }
